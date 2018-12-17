c9cc470f692e7160b191faa92dbd6cae033100b4
Your Name <you@example.com>
2018-12-17 14:14:07 -0500

    MVP

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 export default () => {
     alert('it works!');
   };
+  import './main.css';
+  import Fireworks from 'fireworks-canvas';
+ 
+export default () => {
+  const container = document.getElementById('container');
+  const fireworks = new Fireworks(container);
+  fireworks.start();
+};
